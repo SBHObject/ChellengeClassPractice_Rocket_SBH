d81078fca5af2698e47b716f144d1811b1b99351
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-10-25 12:25:33 +0900

    Add: Q3 구현사항 2, 3
    구현사항2: fov 내부에 소행성 여부 판정
    구현사항3: 소행성 포착시 삼각형이 반짝거리도록 애니메이션 적용

--- ChellengeClass_Rocket/Assets/Scripts/Challenge/AlertSystem.cs
+++ ChellengeClass_Rocket/Assets/Scripts/Challenge/AlertSystem.cs
@@ -15,6 +15,7 @@ public class AlertSystem : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         // FOV를 라디안으로 변환하고 코사인 값을 계산
+        alertThreshold = Mathf.Cos(fov * Mathf.Deg2Rad / 2);
     }
 
     private void Update()
@@ -25,5 +26,24 @@ public class AlertSystem : MonoBehaviour
     private void CheckAlert()
     {
         // 주변 반경의 소행성들을 확인하고 이를 감지하여 Alert를 발생시킴(isBlinking -> true)
+        int layerMask = LayerMask.GetMask("Aesteriod");
+
+        var hits = Physics2D.CircleCastAll(transform.position, radius, Vector2.up, 0f, layerMask);
+
+        bool check = false;
+
+        foreach (var hit in hits)
+        {
+            Vector2 dir = hit.transform.position - transform.position;
+            float cos = Vector2.Dot(dir.normalized, transform.up.normalized);
+            if (cos >= alertThreshold)
+            {
+                //작동
+                check = true;
+                break;
+            }
+        }
+
+        animator.SetBool(blinking, check);
     }
 }
